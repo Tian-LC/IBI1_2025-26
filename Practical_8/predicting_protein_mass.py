71381a5c7f55a84e57d70918897f9925ef80b53e
Tian-LC <linchuan.25@intl.zju.edu.cn>
2026-04-13 06:50:51 +0100

    the first version

---
+++ Practical_8/predicting_protein_mass.py
@@ -0,0 +1,17 @@
+def PPM (t):
+    amino = {'G':57.02, 'A':71.04, 'S':87.03, 'P':97.05, 'V':99.07, 'T': 101.05, 'C':103.01, 'I':113.08, 'L':113.08, 'N':114.04, 'D':115.03, 'Q': 128.06, 'K':128.09, 'E':129.04, 'M':131.04, 'H':137.06, 'F':147.07, 'R':156.10, 'Y':163.06, 'W':186.08}
+    mass = 0
+    for i in t:
+        if i in amino:
+            mass += amino[i]
+        else:
+            return -1
+    return round(mass,2)
+target = input()
+if PPM(target) == -1:
+    print("ERROR: at least one aminos are not be defiend")
+else:
+    print(PPM(target))
+##calling example
+#input: GGGFAA(any protein sequence)
+#output: predicted mass of the protein sequence
