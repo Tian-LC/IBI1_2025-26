973a86ad7cf04503706a51137130903ca92bd7bf
Tian-LC <linchuan.25@intl.zju.edu.cn>
2026-05-11 17:19:29 +0100

    correct the data validity-check commands

--- Practical_4/creatine_clearance.py
+++ Practical_4/creatine_clearance.py
@@ -14,23 +14,34 @@
 #         print("variable "creatinine concentration" needs corrected")
 #     if gender neither "male" nor "female":
 #         print("variable "gender" needs corrected")
-
+#1. input data
 age = float(input("Please input age:"))
 weight = float(input("Please input weight:"))
 Cr = float(input("Please input creatinine concentration:"))
 gender = input("Please input gender:")
-if age < 100 and weight > 20 and weight < 80 and Cr > 0 and Cr < 100 and gender == "male" or "female":
+#2. check the validity of inputed data
+is_valid = True
+
+if age >= 100:
+    print("variable 'age' needs to be corrected")
+    is_valid = False
+
+if weight <= 20 or weight >= 80:
+    print("variable 'weight' needs to be corrected")
+    is_valid = False
+
+if Cr <= 0 or Cr >= 100:
+    print("variable 'creatinine concentration' needs to be corrected")
+    is_valid = False
+
+if gender not in ("male", "female"):
+    print("variable 'gender' needs to be corrected")
+    is_valid = False
+
+#3. only calculate when the input is valid
+if is_valid:
     if gender == "male":
-        CrCl = ((140-age) * weight) / (72*Cr)
+        CrCl = ((140 - age) * weight) / (72 * Cr)
     else:
-        CrCl = ((140-age) * weight) * 0.85 / (72*Cr) 
+        CrCl = ((140 - age) * weight) * 0.85 / (72 * Cr)
     print("CrCl is", CrCl)
-else:
-    if age >= 100:
-        print("variable 'age' needs corrected")
-    if weight > 80 or weight < 20:
-        print("variable 'weight' needs corrected")
-    if Cr <= 0 or Cr >= 100:
-        print("variable 'creatinine concentration' needs corrected")
-    if gender != "male" and gender != "female":
-        print("variable 'gender' needs corrected")
