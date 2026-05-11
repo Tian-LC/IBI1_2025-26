125734c7c807c7353e7a731777061cd13b98efc4
Tian-LC <linchuan.25@intl.zju.edu.cn>
2026-05-11 18:47:01 +0100

    correct commands len(i) & add comment lines

--- Practical_7/largest_orf.py
+++ Practical_7/largest_orf.py
@@ -1,12 +1,20 @@
 import re
-seq = 'AUGAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
+seq = 'AAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
+# Find all possible ORFs that start with AUG and end with one of the stop codons: UAA, UAG, or UGA.
 result = re.findall(r'(?=(AUG(?:...)*?(?:UAA|UAG|UGA)))', seq)
+# to store the longest ORF found 
 largest = ''
+# Store all ORFs that have the same longest length
 same_tmp = []
+# Check each ORF found by the regular expression
 for i in result:
+    # If the current ORF is longer than the previous longest ORF,
+    # update the longest ORF and reset the list
     if len(largest) < len(i):
         largest = i
         same_tmp = [i]
-    if len(largest) == i:
+    # If the current ORF has the same length as the longest ORF,
+    # add it to the list
+    elif len(largest) == len(i):
          same_tmp.append(i)  
-print('the largest orf(s):', same_tmp, "\n its(their) lenth:", len(largest) )
+print('the largest orf(s):', same_tmp, "\n its(their) length:", len(largest) )
